928ee725eb2722c9f6938187505ff748eb97fb4f
SMVasista <sumanthvasishta@gmail.com>
2017-07-27 00:01:02 +0530

    Create transformDisease.py

---
+++ transformDisease.py
@@ -0,0 +1,13 @@
+from __future__ import division
+import math as mt
+import numpy as NP
+import sys, re, os, random, pickle
+import cwgutils
+
+def parseExpressionData(mxpfile):
+  
+
+
+if __name__=="__main__":
+  script, mxpFile, indication = sys.argv
+  
