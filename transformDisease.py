546fee406d77c587f53da74de6dd4f6cbcc855cf
SMVasista <sumanthvasishta@gmail.com>
2017-07-27 00:28:44 +0530

    Update transformDisease.py

--- transformDisease.py
+++ transformDisease.py
@@ -8,12 +8,17 @@ with open('./NVD.data','r+') as f:
   W = pickle.load(f)
 with open('./COM.data','r+') as f:
   C = pickle.load(f)
+with open('./1711.bdt','r+') as f:
+  BDT = pickle.load(f)
 
 def captureBiomarkerData(mxpfile):
   
 
 def parseExpressionData(mxpfile):
-
+  dataPts = cwgutils.countCSV() - 1
+  expVars = cwgutils.readColumn(mxpfile, 1, ' ')
+  for var in expVars:
+      
 
 if __name__=="__main__":
   script, mxpFile, indication = sys.argv
